ac5afa4252737edb51da7b1a0b518a4aca89c3b3
MasudSultan4 <mdsarwarjahanmasudsultan1971@gmail.com>
2022-01-01 10:15:48 +0600

    this is a blog site website

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      poppins: "'poppins',sans-serif",
+      roboto: "'Roboto',sans-serif"
+    },
+    extend: {
+  
+    },
+  },
+  plugins: [],
+}
